f7e6c74528eeb7a9a12479b27ed395046c716af1
lee <leeco.work@gmail.com>
2014-08-18 15:39:35 +0300

    added fixes

--- models/desiredApt.js
+++ models/desiredApt.js
@@ -2,21 +2,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var DesiredAptSchema = new mongoose.Schema({
-	creator : { type: Schema.ObjectId, ref: 'Users' 
+	creator : { type: Schema.ObjectId, ref: 'Users' },
 	city: {type: String, required: true},
 	Neighborhood: {type: String},
 	Street: {type: String},
-	From_HouseNumber: {type: int},
-	To_HouseNumber: {type: int},
-	Max_Price: {type: int},
-	Size: {type: int},
-	Max_Rooms: {type: int},
-	Min_Rooms: {type: int},
-	Max_Level: {type: int},
-	Min_Level: {type: int},
-	Parking: {type: int},
-	Elevator: {type: int},
-	Pets: {type: int},
+	From_HouseNumber: {type: Number},
+	To_HouseNumber: {type: Number},
+	Max_Price: {type: Number},
+	Size: {type: Number},
+	Max_Rooms: {type: Number},
+	Min_Rooms: {type: Number},
+	Max_Level: {type: Number},
+	Min_Level: {type: Number},
+	Parking: {type: Number},
+	Elevator: {type: Number},
+	Pets: {type: Number},
 	Published_Date: {type: Date},
 });
 
