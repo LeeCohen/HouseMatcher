745204a2b84293079deb5d7b02a418c6cc33994e
oriaratzon <oriaratzon1@gmail.com>
2014-08-19 14:33:27 +0300

    offerdApt js updated and html added

---
+++ models/test.js
@@ -0,0 +1,7 @@
+/*var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var DesiredAptSchema = new mongoose.Schema();
+var OfferdAptSchema = new Schema();
+var UsersSchema = new Schema();*/
+var sys = require("sys");  
+sys.puts("Hello World"); 
