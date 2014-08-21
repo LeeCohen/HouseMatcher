44f37d1dd130a7f33ff65ef4aaed0e85c1853d0c
oriaratzon <oriaratzon1@gmail.com>
2014-08-21 13:04:33 +0300

    HTML 2

--- models/user.js
+++ models/user.js
@@ -1,15 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-function emailValidator(elementValue){        
-    var emailPattern = /^[a-zA-Z0-9._]+[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$/;  
-    return emailPattern.test(elementValue);   
-  } 
-
 var UsersSchema = new Schema({
   username: {type: String, required: true},
   Hashed_password: {type: String, required: true},
-  Email: {type: String, required: true, validate: emailValidator},
+  Email: {type: String, required: true},
   Phone_Number: {type: String, required: true},
   First_name: {type: String, required: true},
   First_name: {type: String, required: true}
